5e7220b31ddd596e1a34218d9908ecb097a3b26d
season19840122 <season19840122@163.com>
2021-08-09 22:27:38 +0800

    update 第1节  01 开发环境准备

---
+++ 全栈工程师进阶课/第一章 Vue 预习视频/第1节  01 开发环境准备/exercise/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        computed: {
+            name() {
+                return this.data
+            }
+        },
+        methods: {
+            name() {
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
